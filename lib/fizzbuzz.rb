26a741b36039746fa08b26a207d53cd871824be6
ayoub <ayoub.hamdoon@gmail.com>
2023-01-30 14:06:25 +0000

    write test cases and finish fizzbuzz method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,15 @@
 def fizzbuzz(num)
-    'fizz'
+    if num % 3 == 0 && num % 5 == 0
+        return "fizzbuzz"
+    elsif num % 3 == 0
+        return "fizz"
+    elsif num % 5 == 0
+        return "buzz"
+    else
+        return num
+    end
+end
+
+(1..100).each do |number|
+    puts fizzbuzz(number)
 end
